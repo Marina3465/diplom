9141ff7f5baccd48021c07e3db96e7e7c433005c
Marina Kozhukhar <kozhyxarm12@gmail.com>
2024-01-09 21:33:22 +0300

    добавлен функционал поиска

--- src/components/admin/Admin_main.js
+++ src/components/admin/Admin_main.js
@@ -1,31 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import './Admin_main.css';
 import Admin_header from './Admin_header';
+
 const endpoint = 'https://jsonplaceholder.typicode.com/users';
 
-
 function Admin_main() {
-  const [users, setUsers] = useState([])
+  const [allUsers, setAllUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    (async () => {
-      const data = await fetch(endpoint)
-        .then(res => res.json())
+    const fetchData = async () => {
+      try {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        setAllUsers(data);
+        setFilteredUsers(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-      setUsers(data)
-    })()
-  }, [])
+    fetchData();
+  }, []);
+
+  const handleSearch = () => {
+    const filtered = allUsers.filter(user =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  };
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+    const filtered = allUsers.filter(user =>
+      user.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  };
 
   return (
-    <><Admin_header />
+    <>
+      <Admin_header />
       <div className='admin-main-search'>
-        <input type='text' />
-        <button className='button-search'>
-          <img src={require('../../img/search.png')} />
-        </button>
+        <input
+          type='text'
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder='Поиск по имени...'
+        />
       </div>
 
-      {users.map(user => (
+      {filteredUsers.map(user => (
         <div className='cart' key={user.id}>
           <div className='data'>
             {user.id}
@@ -38,17 +64,16 @@ function Admin_main() {
               <p><span>Статус:</span> {user.address.zipcode}</p>
             </div>
             <div className='col2'>
-              <p><span>Предмет:</span> {user.address.city}</p>
+              <p><span>Дисциплина:</span> {user.address.city}</p>
               <p><span>Преподаватель:</span> {user.email}</p>
               <p><span>Кафедра:</span> {user.address.street}</p>
               <p><span>Название:</span> {user.company.catchPhrase}</p>
             </div>
           </div>
-        </div>
-
+          </div>
       ))}
     </>
-  )
+  );
 }
 
-export default Admin_main
+export default Admin_main;
