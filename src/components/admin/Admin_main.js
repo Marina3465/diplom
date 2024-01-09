454796e2118f62b46eada512eed205f7f74a6730
Marina Kozhukhar <kozhyxarm12@gmail.com>
2024-01-09 11:08:24 +0300

    выведены данные и создан поиск на главной админа

--- src/components/admin/Admin_main.js
+++ src/components/admin/Admin_main.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Admin_main.css';
 import Admin_header from './Admin_header';
-// import employee from '../../employee.json'
 const endpoint = 'https://jsonplaceholder.typicode.com/users';
 
 
@@ -19,36 +18,35 @@ function Admin_main() {
 
   return (
     <><Admin_header />
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name (Username)</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Website</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name} ({user.username})</td>
-              <td>
-                <a href={`mailto:${user.email}`}>
-                  {user.email}
-                </a>
-              </td>
-              <td>{user.phone}</td>
-              <td>
-                <a href={`https://${user.website}`} target="_blank">
-                  {user.website}
-                </a>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className='admin-main-search'>
+        <input type='text' />
+        <button className='button-search'>
+          <img src={require('../../img/search.png')} />
+        </button>
+      </div>
+
+      {users.map(user => (
+        <div className='cart' key={user.id}>
+          <div className='data'>
+            {user.id}
+          </div>
+          <div className='content'>
+            <div className='col1'>
+              <p><span>ФИО:</span> {user.name}</p>
+              <p><span>Группа:</span> {user.address.suite}</p>
+              <p><span>Тип работы:</span> {user.company.name}</p>
+              <p><span>Статус:</span> {user.address.zipcode}</p>
+            </div>
+            <div className='col2'>
+              <p><span>Предмет:</span> {user.address.city}</p>
+              <p><span>Преподаватель:</span> {user.email}</p>
+              <p><span>Кафедра:</span> {user.address.street}</p>
+              <p><span>Название:</span> {user.company.catchPhrase}</p>
+            </div>
+          </div>
+        </div>
+
+      ))}
     </>
   )
 }
